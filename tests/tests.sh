0116f33a31b3cc6fef82e723c6e007f78960e6ab
Eric <93eric@gmail.com>
2022-12-21 10:17:38 +0100

    Added tests

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,3 +1,68 @@
-echo "Test with 8 characters"
-./client $1 "Hello 42"; 2>&1
+#!/bin/bash
+
+ARG=$(pidof ./server)
+cien="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in nulla sed orci sagittis lacinia vel ac orci. Proin 
+finibus semper tellus, nec vestibulum libero hendrerit nec. Integer vitae massa at est lobortis pulvinar at eget leo. 
+Nullam eu mauris in dolor ultrices fermentum. Ut aliquam, arcu nec consectetur hendrerit, risus eros sagittis metus, a 
+ultrices metus ipsum et mauris. Etiam eu cursus mi, a imperdiet magna. Etiam nec molestie nibh. Mauris vel hendrerit est.
+
+Cras nulla massa, fringilla vitae arcu at, ornare vestibulum nisl. Cras nisi urna, auctor convallis vulputate vel, sodales et 
+ligula. Aliquam ut efficitur mauris, in."
+
+mil="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel rhoncus purus. Aliquam est nulla, lobortis sit amet facilisis sed, pretium id nibh. Pellentesque molestie posuere tincidunt. Fusce vel tincidunt nunc. Donec mattis erat vel consectetur tincidunt. Mauris vel quam a nibh maximus tincidunt. Vivamus tincidunt orci lectus, porttitor rhoncus eros commodo consectetur. Mauris non sollicitudin tortor. Fusce odio lorem, vehicula et tincidunt porttitor, interdum non nisl. Proin vestibulum molestie varius. Sed pretium, libero id pulvinar consectetur, nibh purus rhoncus odio, non tincidunt diam lectus ac leo. Pellentesque tincidunt velit ut massa posuere egestas. Fusce euismod eros sit amet sem scelerisque, vel ornare mi blandit.
+
+Aliquam leo massa, tristique nec dui ac, vulputate ultrices enim. Maecenas venenatis, turpis vel suscipit aliquam, mauris nulla dictum diam, ut facilisis odio sapien a sem. Quisque maximus imperdiet massa, vel posuere est fermentum eu. Aliquam vitae dapibus lacus. Mauris accumsan rutrum magna, at tempus nunc dignissim nec. Morbi quis aliquam est, quis congue magna. Integer rutrum suscipit tortor, vitae efficitur erat porttitor a. Maecenas convallis nec diam sed vehicula. Integer eget orci eget ex congue fringilla vitae id tortor. Ut venenatis justo diam. Aliquam erat volutpat. Donec et risus pretium, rhoncus felis iaculis, laoreet ex.
+
+Etiam sed urna risus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam mollis placerat placerat. Proin nisl sem, dictum quis risus sit amet, feugiat vulputate erat. Etiam ullamcorper nibh vitae quam euismod, quis tincidunt nisi volutpat. Suspendisse venenatis nunc est, id porttitor tellus vulputate quis. Suspendisse lectus nunc, pulvinar in accumsan ac, tincidunt non risus. Suspendisse gravida tincidunt orci. In feugiat lacinia massa ac congue. Quisque sit amet odio a sem congue porta et a mi. Nunc rhoncus metus nisi, et auctor libero ullamcorper ut. Ut a feugiat lorem.
+
+Sed et consectetur urna, a porta metus. Morbi euismod mi mi, vel lobortis nulla semper hendrerit. Etiam laoreet lorem non justo sagittis tincidunt. Morbi egestas orci nec libero ornare, vitae suscipit urna rutrum. Morbi justo tortor, fermentum vitae enim quis, mattis viverra sem. Proin eget rhoncus orci, ut suscipit ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut cursus ornare tortor eu aliquam. Duis a sollicitudin enim, sit amet lacinia lacus. In vitae odio pharetra, finibus orci sed, scelerisque enim. Mauris mollis purus quam, at blandit ante vehicula quis. Praesent sed aliquet arcu, nec placerat leo. Nam non urna in purus tempus pulvinar.
+
+In quis odio metus. Quisque tellus dolor, rhoncus id magna lobortis, tincidunt pulvinar magna. Vivamus ut eleifend neque, vel malesuada eros. Proin dui quam, maximus vitae nibh non, sodales pretium enim. Vivamus fringilla nibh quis ligula rhoncus consequat. Aliquam vehicula nibh mi, eget rhoncus tellus elementum vitae. Nulla non nunc at lorem pharetra accumsan vitae sit amet velit. In ornare mauris massa, sit amet viverra sapien tincidunt nec. Nunc non sapien nec libero aliquam efficitur. Sed vel orci et metus facilisis cursus. Etiam scelerisque tortor ut purus suscipit, sed lobortis neque convallis.
+
+Cras pretium sollicitudin arcu, vel vulputate enim rhoncus sit amet. Sed lectus diam, luctus quis vestibulum a, consequat iaculis erat. Quisque fringilla mauris velit, sed tempor justo posuere quis. Quisque at ipsum eget orci lacinia varius. Nunc ac odio sagittis, suscipit sem rutrum, dictum ante. Duis sed quam viverra, euismod dolor non, lacinia enim. Sed mollis quam semper leo ullamcorper, sed egestas quam semper. Maecenas faucibus rutrum ipsum, vel eleifend quam vestibulum eget.
+
+Aenean vel tortor tincidunt, pharetra risus in, aliquam mi. In hac habitasse platea dictumst. Phasellus auctor odio id lorem commodo pharetra. Cras id diam augue. Aenean nisl mauris, tempor sed molestie at, rhoncus ac sapien. Duis nec cursus tortor. Mauris ac posuere nunc, eu finibus velit. Morbi viverra nibh sit amet dolor aliquam sollicitudin in vitae dui. Maecenas mi lorem, tincidunt ac placerat sit amet, placerat dignissim ligula. Nunc lectus risus, tincidunt eu tellus sed, faucibus euismod purus. Phasellus rhoncus dictum quam eu venenatis. Nulla dapibus pellentesque ex vel imperdiet.
+
+Suspendisse potenti. Integer finibus luctus libero, id vulputate turpis dignissim facilisis. Aenean cursus, turpis in blandit interdum, lorem nunc facilisis risus, vel tempor mi tellus non nunc. Aliquam faucibus risus eget varius pellentesque. Integer lobortis vulputate euismod. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer at viverra lectus, non luctus enim. Aliquam scelerisque sagittis faucibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis bibendum magna quis nulla suscipit, eu pharetra justo consequat.
+
+Curabitur a tincidunt augue. Nunc varius malesuada elit, non pellentesque mi gravida accumsan. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras ut sodales mi. Sed ornare in odio vitae semper. Proin hendrerit risus sed tellus rutrum fringilla. Etiam sit amet nisl id tortor ornare auctor vitae non nunc. Ut in pretium ligula, vitae venenatis mauris. Mauris euismod dignissim arcu nec tristique. Integer sed risus quis lacus blandit rhoncus ac nec felis. Aenean leo libero, sollicitudin a est ut, tempor porttitor lectus. Duis metus magna, tempor eu molestie quis, porta tristique nulla. Etiam nec velit at dui congue dapibus. Sed pharetra nibh sit amet urna luctus varius.
+
+Phasellus quis mauris id tellus condimentum pharetra eu in tortor. In tincidunt, arcu at dictum tincidunt, magna est ornare nunc, eu varius orci quam sit amet tortor. In vulputate nunc quis hendrerit eleifend. Sed scelerisque libero non quam commodo fermentum. Donec id maximus risus. Nullam egestas eu velit id tristique. Curabitur tristique porta lectus sit amet dignissim. Morbi id nibh in orci cursus venenatis sit amet in ex. Proin nec ex magna. In non placerat magna.
+
+In sit amet odio pellentesque, ultricies nibh eu, aliquam libero. Donec ullamcorper mauris vitae odio fringilla imperdiet. Cras non vulputate massa. Aliquam elit nisl, aliquet ut nulla in, egestas facilisis urna. Proin id commodo nisl, nec varius lectus. Proin quis rutrum lacus. Aenean at odio eu justo suscipit mattis. Duis porta felis in aliquam feugiat. In hac habitasse platea dictumst. Phasellus non lacus nisi. Aliquam vel convallis diam. Sed semper, tortor at efficitur fermentum, libero nunc."
+
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
+BOLD="\033[0;1m"
+RESET="\033[0;0m"
+
+echo "
+.............................................
+.               STARTING TESTS              .
+.............................................
+"
+
+echo "${YELLOW}[TEST 1] Wrong pid${RESET} --> Should return error"
+./client 0 "Wrong pid"; 2>&1
 echo
+
+echo "${YELLOW}[TEST 2] Correct pid with characters${RESET} --> Should return error"
+./client "$ARG/a" "Wrong pid"; 2>&1
+echo
+
+echo "${YELLOW}[TEST 3] Null pid${RESET} --> Should return error"
+./client "" "Wrong pid"; 2>&1
+echo
+
+echo "${YELLOW}[TEST 4] Test with 8 characters${RESET} --> Server should return string"
+./client $ARG "Hello 42"; 2>&1
+echo
+
+echo "${YELLOW}[TEST 5] Test with 100 characters${RESET} --> Server should return string"
+./client $ARG "$cien"; 2>&1
+echo
+
+echo "${YELLOW}[TEST 6] Test with 1000 characters${RESET} --> Server should return string"
+./client $ARG "$mil"; 2>&1
+echo
+
